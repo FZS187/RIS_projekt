93546043ec05ca4eb98b4249e4199a5e1f98a2c5
FZS187 <sekulovicf@gmail.com>
2025-12-07 18:00:50 +0100

    Gotov backend za login i registration page

--- todo-backend/src/main/java/com/example/todobackend/controller/TodoController.java
+++ todo-backend/src/main/java/com/example/todobackend/controller/TodoController.java
@@ -9,7 +9,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/todos")
-@CrossOrigin(origins = "http://localhost:5173")
+@CrossOrigin(origins = "http://localhost:5173", allowCredentials = "true")
 public class TodoController {
 
     @Autowired
